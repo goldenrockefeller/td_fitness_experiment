05c1ffe4ca475c7ae315202e977e090367b58ced
Golden Rockefeller <golden.rockefeller@protonmail.com>
2021-05-24 18:43:36 -0700

    Add log data for B-SARSA in the TD0 experiment

---
+++ log/TD0/run.py
@@ -0,0 +1,48 @@
+from td_fitness import *
+from runner import *
+from mods import *
+import itertools
+import random
+from multiprocessing import Process
+from time import sleep
+import sys
+
+
+def run():
+    experiment_name = "TD0"
+    n_stats_run_per_process = 1
+
+
+    mods_to_mix = [
+        (biqtc, biqsc),
+    ]
+
+
+    runners = [
+        Runner(experiment_name, setup_combo)
+        for setup_combo in itertools.product(*mods_to_mix)
+    ]
+
+    random.shuffle(runners)
+
+    for i in range(n_stats_run_per_process):
+        for runner in runners:
+            runner.new_run()
+
+
+
+if __name__ == '__main__':
+    # r = Runner('test', (biqtc,))
+    # r.new_run()
+    n_processes = int(sys.argv[1])
+    print(f"Number of processes: {n_processes}")
+
+    processes = [Process(target = run) for _ in range(n_processes)]
+
+    for process in processes:
+        process.start()
+        sleep(2)
+
+
+    for process in processes:
+        process.join()
